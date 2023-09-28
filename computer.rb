14550d4b1f72b4968568a5fe871e0b2eedcc0c5c
harry <harry_emmett@outlook.com>
2023-09-29 00:14:40 +0100

    fix: repo

--- computer.rb
+++ computer.rb
@@ -4,13 +4,16 @@
 class Computer
   def initialize(difficulty)
     @difficulty = difficulty
-    @board = Board.new
+    # @board = Board.new
     @current_board = nil
+    @board = ['X', 'O', 'X', 'O', ' ', ' ', ' ', 'O', ' ']
+
   end
 
   def play_computer(c)
     self.current_board = c
-    difficulty == 'easy' ? easy_game(c) : hard_game(c)
+    p best_move(board)
+    # difficulty == 'easy' ? easy_game(c) : hard_game(c)
   end
 
   private
@@ -18,64 +21,93 @@ class Computer
   attr_reader :difficulty
   attr_accessor :current_board, :board
 
-  def empty_index(b)
-    b.select { |num| num != 'x' && num != 'o' }
-  end
-
   def winning(b, player)
     winning_combiniations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     winning_combiniations.any? do |val|
-      return true if [b[val[0]], b[val[1]], b[val[2]]].uniq.length == 1 && b[val[0]] == player
+      return player if [b[val[0]], b[val[1]], b[val[2]]].uniq.length == 1 && b[val[0]] == player
     end
     false
   end
 
-  def minimax(b, player = 'x')
-    available = empty_index(b)
-    hash = {}
+  # def hard_game(b)
+  #   p find_best_move(b)
+  #   find_best_move(b)
+  # end
 
-    if winning(b, 'x')
-      hash['score'] = 10
-      return hash
-    elsif winning(b, 'o')
-      hash['score'] = -10
-      return hash
-    elsif available.length == 0
-      hash['score'] = 0
-      return hash
+  def available_moves
+    moves = []
+    board.each_with_index do |cell, i|
+      moves << i if cell == ' '
     end
+    moves
+  end
 
-    movesa = []
+  def make_move(player, move)
+    board[move] = player
+  end
 
-    available.each_with_index do |val, _inx|
-      move = {}
-      moveb = {}
-      b[val.to_i] = player
+  def undo_move(move)
+    board[move] = ' '
+  end
 
-      # if player == 'x'
-      #   result = minimax(b, 'o') || nil
-  
-      #   return unless result
+  def winner
+    winning_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
-      #   move['index'] = val
-      #   move['score'] = result['score']
-      #   movesa << move
-      #   b[val.to_i] = val
-      # else
-        result = minimax(b, player == "x" ? "o" : "x") || nil
-        return unless result
-        moveb['index'] = val
-        moveb['score'] = result['score']
-        movesa << moveb
-        b[val.to_i] = val
-  
-      # end
-
+    winning_combinations.each do |combo|
+      if board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && board[combo[0]] != ' '
+        return board[combo[0]]
+      end
     end
-    p movesa
 
     nil
-    # moves.push(win)
+  end
+
+  def game_over?
+    winner || board.none?(' ')
+  end
+
+  def minimax(board, depth, maximizing_player)
+    return 1 if winner == 'X'
+    return -1 if winner == 'O'
+    return 0 if game_over?
+
+    if maximizing_player
+      max_eval = -Float::INFINITY
+      available_moves.each do |move|
+        make_move('X', move)
+        eval = minimax(board, depth + 1, false)
+        undo_move(move)
+        max_eval = [max_eval, eval].max
+      end
+      max_eval
+    else
+      min_eval = Float::INFINITY
+      available_moves.each do |move|
+        make_move('O', move)
+        eval = minimax(board, depth + 1, true)
+        undo_move(move)
+        min_eval = [min_eval, eval].min
+      end
+      min_eval
+    end
+  end
+
+  def best_move(board)
+    best_score = -Float::INFINITY
+    best_move = nil
+
+    available_moves.each do |move|
+      make_move('X', move)
+      score = minimax(board, 0, false)
+      undo_move(move)
+
+      if score > best_score
+        best_score = score
+        best_move = move
+      end
+    end
+
+    best_move
   end
 
   def easy_game(b)
@@ -84,95 +116,4 @@ class Computer
     p "Computer chooses #{pick}"
     pick - 1
   end
-
-  def hard_game(b)
-    # i = b.map do |x|
-    #   if %w[x o].include?(x)
-    #     x
-    #   else
-    #     (x.to_i - 1).to_s
-    #   end
-    # end
-
-    p minimax(b)
-    # p "Computer chooses #{test['index']}"
-    # test['index'].to_i
-  end
 end
-
-# available.each_with_index do |_val, index|
-#   move = {}
-#   move['index'] = b[available[index].to_i]
-
-#   # p player
-#   b[available[index].to_i] = player
-
-#   if player == 'x'
-#     result = minimax(b, 'o')
-#     move['player'] = 'x'
-#     move['score'] = if result && result['score']
-
-#                       result['score']
-#                     else
-
-#                       0
-#                       return
-#                     end
-#   else
-#     result = minimax(b, 'x')
-#     move['player'] = 'o'
-#     # p result
-#     move['score'] = if result && result['score']
-#                       result['score']
-#                     else
-#                       0
-#                       return
-#                     end
-#   end
-
-#   b[available[index].to_i] = move['index']
-
-#   moves.push(move)
-# end
-# p moves
-# nil
-# # p moves
-# # best_move = nil
-# # if player == 'x'
-# #   best_score = -1000
-# #   moves.each do |val|
-# #     best_move = val if val['score'] != 0 && val['score'] > best_score
-# #   end
-# # else
-# #   best_scoreb = 1000
-# #   moves.each do |val|
-# #     best_move = val if val['score'] != 0 && val['score'] < best_scoreb
-# #   end
-# # end
-# # best_move['index']
-# end
-
-# for i in 0..available.length - 1 do
-
-#   move = {}
-
-#   move['index'] = b[available[i].to_i]
-
-#   b[available[i].to_i] = player
-
-#   if player == 'x'
-#     result = minimax(b, 'o')
-#     move['player'] = player
-#     val = result['score']
-#     move['score'] = result
-#   else
-#     result = minimax(b, 'x')
-#     move['player'] = player
-#     p result
-#     val = result['score']
-#     move['score'] = result
-#   end
-
-#   b[available[i].to_i] = move['index'].to_s
-
-#   moves.push(move)
--- main.rb
+++ main.rb
@@ -10,11 +10,11 @@ require './player'
 # game.init_game
 
 computer = Computer.new('hard')
-# computer.play_computer(%w[o x o x o x 6 7 8])
+computer.play_computer(%w[x 1 2 3 4 5 6 7 8])
 
-# computer.play_computer(%w[x x x o x o x o o])
-# computer.play_computer(%w[x o x o o x x x o])
-computer.play_computer(%w[x x o x o o 6 7 8])
-# x x o 
-# x o o 
-# 6 7 8
+# # computer.play_computer(%w[x x x o x o x o o])
+# # computer.play_computer(%w[x o x o o x x x o])
+# computer.play_computer(%w[x x o x x o 6 o 8])
+# # x x o 
+# # x x o 
+# # 6 o 8
